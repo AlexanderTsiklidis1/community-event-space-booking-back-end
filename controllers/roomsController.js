b3b8be6558da99a0a3cfcf52f959320efe7460af
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2024-01-27 10:13:36 -0500

    completes roomsController.js file.

---
+++ controllers/roomsController.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { getAllRooms, getOneRoom, createRoom } = require("../queries/rooms.js");
+
+const bookingsController = require("./bookingsController.js");
+
+const rooms = express.Router({ mergeParams: true });
+
+rooms.use("/:roomId/bookings", bookingsController);
+
+rooms.get("/", async (req, res) => {
+  const allRooms = await getAllRooms();
+  if (allRooms[0]) {
+    res.status(200).json(allRooms);
+  } else {
+    res.status(500).json({ success: false, data: { error: "Server Error" } });
+  }
+});
+
+rooms.get("/:index", async (req, res) => {
+  const { index } = req.params;
+  const oneRoom = await getOneRoom(index);
+  if (oneRoom) {
+    res.json(oneRoom);
+  } else {
+    res.status(404).json({ error: "Not Found" });
+  }
+});
+
+rooms.post("/", async (req, res) => {
+  try {
+    const createdRoom = await createRoom(req.body);
+    res.json(createdRoom);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+module.exports = rooms;
