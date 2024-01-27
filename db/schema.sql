464abde7cdeb2c54b57424731249fd22472f3978
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2024-01-27 09:44:47 -0500

    completes both schema.sql & seed.sql files.

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS community_event_dev;
+CREATE DATABASE community_event_dev;
+
+\c community_event_dev;
+
+CREATE TABLE rooms (
+    id SERIAL PRIMARY KEY,
+    room_name TEXT NOT NULL,
+    floor INTEGER,
+    capacity INTEGER
+);
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    booking_name TEXT NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL,
+    attendees TEXT,
+    room_id INTEGER REFERENCES rooms (id) ON DELETE CASCADE
+);
