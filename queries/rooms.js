25e195ddebce2b75ae859470b9dd6d2e1d77ab73
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-01-28 11:41:50 -0500

    Add id in rooms.js.

--- queries/rooms.js
+++ queries/rooms.js
@@ -1,37 +1,37 @@
-const db = require("../db/dbConfig.js");
+const db = require('../db/dbConfig.js');
 
 const getAllRooms = async () => {
-  try {
-    const allRooms = await db.any("SELECT * FROM rooms");
-    return allRooms;
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const allRooms = await db.any('SELECT * FROM rooms');
+		return allRooms;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
-const getOneRoom = async () => {
-  try {
-    const oneRoom = await db.one("SELECT * FROM rooms WHERE id=$1", id);
-    return oneRoom;
-  } catch (error) {
-    console.error(error);
-  }
+const getOneRoom = async (id) => {
+	try {
+		const oneRoom = await db.one('SELECT * FROM rooms WHERE id=$1', id);
+		return oneRoom;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 const createRoom = async (room) => {
-  try {
-    const createdRoom = await db.one(
-      "INSERT INTO rooms (room_name, floor, capacity) VALUES ($1, $2, $3) RETURNING *",
-      [room.room_name, room.floor, room.capacity]
-    );
-    return createdRoom;
-  } catch (error) {
-    console.error(error)
-  }
+	try {
+		const createdRoom = await db.one(
+			'INSERT INTO rooms (room_name, floor, capacity) VALUES ($1, $2, $3) RETURNING *',
+			[room.room_name, room.floor, room.capacity]
+		);
+		return createdRoom;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 module.exports = {
-    getAllRooms,
-    getOneRoom,
-    createRoom
+	getAllRooms,
+	getOneRoom,
+	createRoom,
 };
